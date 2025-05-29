4dbb15040966dbc0ee3581e130be7dfb8a983ab6
pedro-scarelli <pscarelli@furb.br>
2025-05-28 22:28:09 -0300

    feat: improved messages in authorizer and added validation on delete user

--- adapter/postgres/accountrepository/delete.go
+++ adapter/postgres/accountrepository/delete.go
@@ -2,18 +2,25 @@ package accountrepository
 
 import (
 	"context"
+
+	"github.com/pedro-scarelli/wheredidmymoneygo/core/domain"
 )
 
 func (repository repository) Delete(accountID string) error {
 	ctx := context.Background()
 
-	_, err := repository.db.Exec(
+	result, err := repository.db.Exec(
 		ctx,
 		`DELETE FROM tb_account
 		WHERE pk_st_id = $1`,
-		accountID)
+		accountID,
+	)
 	if err != nil {
 		return err
+	}
+
+	if result.RowsAffected() == 0 {
+		return domain.ErrAccountNotFound
 	}
 
 	return nil
